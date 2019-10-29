934aeae75e73ebd1c3ce9f41b7d68f1f90db248e
kang-haneul <55428030+kang-haneul@users.noreply.github.com>
2019-10-29 15:46:23 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i;
+	int student[5];
+	
+	student[0]=10;
+	student[1]=20;
+	student[2]=30;
+	student[3]=40;
+	student[4]=50;
+	
+	for(i=0;i<5;i++)
+		printf("student[%i]=%i\n",i,student[i]);
+		
+		
+	
+	
+	return 0;
+}
